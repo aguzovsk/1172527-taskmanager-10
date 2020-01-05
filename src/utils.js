47c266dd7c0d2c097bb3ee55c9877554881b8eac
anton Guzovskiy <antonguzowski@gmail.com>
2020-01-05 13:33:59 +0100

    Module 4 task 1 completed

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,8 @@
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
 const castTimeFormat =
   (value) => value < 10 ? `0${value}` : String(value);
 
@@ -7,4 +12,22 @@ export const formatTime = (date) => {
   const period = date.getHours() > 11 ? `pm` : `am`;
 
   return `${hours}:${minutes} ${period}`;
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
+export const render = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
 };
